681b5d60ab5c232511f43423b1fc4aae3c06c372
h20170047 <46812751+h20170047@users.noreply.github.com>
2022-12-09 15:05:32 +0530

    re-instantiating future-date constraint

--- src/test/java/com/svj/integrationTests/OptiKartApplicationTests.java
+++ src/test/java/com/svj/integrationTests/OptiKartApplicationTests.java
@@ -5,6 +5,7 @@ import com.svj.dto.CustomerRequestDTO;
 import com.svj.dto.CustomerResponseDTO;
 import com.svj.dto.ServiceResponse;
 import com.svj.entity.CustomerEntity;
+import com.svj.utilities.AppUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -28,6 +30,7 @@ class OptiKartApplicationTests {
 	private int port;
 	private String baseUrl="http://localhost:";
 	private static TestRestTemplate restTemplate;
+	private LocalDateTime futureDate= LocalDateTime.parse(LocalDate.now().plusDays(1).toString(), dateTimeFormatter);
 	@Autowired
 	private TestH2Repository repository;
 
@@ -43,7 +46,7 @@ class OptiKartApplicationTests {
 	@Test
 	@Sql(statements = "DELETE FROM ORDERS_TBL WHERE first_Name='John'", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 	public void testAddCustomer() {
-		CustomerRequestDTO customer = new CustomerRequestDTO("John", "Doe" , LocalDateTime.parse("1990-01-01", dateTimeFormatter));
+		CustomerRequestDTO customer = new CustomerRequestDTO("John", "Doe" , futureDate);
 		ServiceResponse serverResponse = restTemplate.postForObject(baseUrl, customer, ServiceResponse.class);
 		final CustomerResponseDTO customerResponse = objectMapper.convertValue(serverResponse.getResponse(), CustomerResponseDTO.class);
 
@@ -52,7 +55,7 @@ class OptiKartApplicationTests {
 	}
 
 	@Test
-	@Sql(statements = "INSERT INTO ORDERS_TBL (id, first_Name, last_Name, date_Time_Of_Purchase) VALUES (2, 'John', 'Doe', '2000-01-01')", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "INSERT INTO ORDERS_TBL (id, first_Name, last_Name, date_Time_Of_Purchase) VALUES (1, 'John', 'Doe', '2000-01-01')", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 	@Sql(statements = "DELETE FROM ORDERS_TBL WHERE first_Name='John'", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 	public void testGetProducts() {
 		 ServiceResponse serverResponse= restTemplate.getForObject(baseUrl, ServiceResponse.class);
@@ -62,37 +65,37 @@ class OptiKartApplicationTests {
 	}
 
 	@Test
-	@Sql(statements = "INSERT INTO ORDERS_TBL (id, first_Name, last_Name, date_Time_Of_Purchase) VALUES (3, 'John', 'Doe', '2000-01-01')", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-	@Sql(statements = "DELETE FROM ORDERS_TBL WHERE id=3", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(statements = "INSERT INTO ORDERS_TBL (id, first_Name, last_Name, date_Time_Of_Purchase) VALUES (1, 'John', 'Doe', '2000-01-01')", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "DELETE FROM ORDERS_TBL WHERE id=1", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 	public void testFindProductById() {
-		ServiceResponse customerResponse = restTemplate.getForObject(baseUrl + "/{id}", ServiceResponse.class, 3);
+		ServiceResponse customerResponse = restTemplate.getForObject(baseUrl + "/{id}", ServiceResponse.class, 1);
 		CustomerResponseDTO customer= objectMapper.convertValue(customerResponse.getResponse(), CustomerResponseDTO.class);
 		assertAll(
 				() -> assertNotNull(customer),
-				() -> assertEquals(3, customer.getId()),
+				() -> assertEquals(1, customer.getId()),
 				() -> assertEquals("John", customer.getFirstName())
 		);
 	}
 
 	@Test
-	@Sql(statements = "INSERT INTO ORDERS_TBL (id, first_Name, last_Name, date_Time_Of_Purchase) VALUES (4, 'John', 'Doe', '2000-01-01')", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-	@Sql(statements = "DELETE FROM ORDERS_TBL WHERE id=4", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+	@Sql(statements = "INSERT INTO ORDERS_TBL (id, first_Name, last_Name, date_Time_Of_Purchase) VALUES (1, 'John', 'Doe', '2000-01-01')", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "DELETE FROM ORDERS_TBL WHERE id=1", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 	public void testUpdateProduct(){
-		CustomerRequestDTO newCustomer = new CustomerRequestDTO("John", "Doe" , LocalDateTime.parse("1990-01-01", dateTimeFormatter));
-		restTemplate.put(baseUrl+"/{id}", newCustomer, 4);
-		CustomerEntity customerFromDB = repository.findById(4).get();
+		CustomerRequestDTO newCustomer = new CustomerRequestDTO("John", "Doe" , futureDate);
+		restTemplate.put(baseUrl+"/{id}", newCustomer, 1);
+		CustomerEntity customerFromDB = repository.findById(1).get();
 		assertAll(
 				() -> assertNotNull(customerFromDB),
-				() -> assertEquals(LocalDateTime.parse("1990-01-01", dateTimeFormatter), customerFromDB.getDateTimeOfPurchase())
+				() -> assertEquals(LocalDateTime.parse(futureDate.toLocalDate().toString(), dateTimeFormatter), customerFromDB.getDateTimeOfPurchase())
 		);
 	}
 
 	@Test
-	@Sql(statements = "INSERT INTO ORDERS_TBL (id, first_Name, last_Name, date_Time_Of_Purchase) VALUES (5, 'John', 'Doe', '2000-01-01')", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+	@Sql(statements = "INSERT INTO ORDERS_TBL (id, first_Name, last_Name, date_Time_Of_Purchase) VALUES (1, 'John', 'Doe', '2000-01-01')", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 	public void testDeleteProduct(){
 		int recordCount=repository.findAll().size();
 		assertEquals(1, recordCount);
-		restTemplate.delete(baseUrl+"/{id}", 5);
+		restTemplate.delete(baseUrl+"/{id}", 1);
 		assertEquals(0, repository.findAll().size());
 
 	}
